55a023c0e37d74043c3e708e1a3b14710d6ddb42
Jacarri Chan <772929989@qq.com>
2015-05-14 14:50:20 +0800

    init
    Signed-off-by: Jacarri Chan <772929989@qq.com>

---
+++ src/main/java/util/PageImfo.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageImfo {
+
+	int pageSum;//锟斤拷页锟斤拷锟斤拷锟斤拷
+	int pageRecordCount;//每页锟侥硷拷录锟斤拷目
+	int pageperfo;//要展示锟斤拷页锟斤拷
+	List<String> Imfor  = new ArrayList<String>();//锟斤拷锟斤拷锟斤拷锟芥将要展示锟斤拷锟斤拷锟?	
+	
+	public List<String> getPageImfo(String showPage,String nameoftbl,String record) {//record使锟斤拷锟剿斤拷锟斤拷模锟斤拷锟窖拷锟斤拷侄锟絥ameoftbl锟角斤拷要锟斤拷询锟侥憋拷锟斤拷锟斤拷锟?
+		
+		
+		
+		return new ArrayList();
+	}
+}
